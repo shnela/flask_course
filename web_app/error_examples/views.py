375b3603c09b49df02ef96d5dbd1852588696854
Jakub Kuszneruk <jkuszneruk@protonmail.com>
2020-12-04 23:51:58 +0100

    Error code handling and static files - solution

--- web_app/error_examples/views.py
+++ web_app/error_examples/views.py
@@ -15,3 +15,8 @@ def abort_response_with_code(response_code):
 @bp.errorhandler(404)
 def page_not_found(e):
     return render_template('errors/404.html'), 404
+
+
+@bp.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
